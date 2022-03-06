fb97ba054ffd39259f0169823ce4b30ddeb49a7d
Dari Alves <darieltondf@gmail.com>
2022-03-06 20:54:03 -0300

    Update index.js

--- 000/index.js
+++ 000/index.js
@@ -1,1 +1,13 @@
 // 05/03/2022
+
+/*
+    Crie uma função chamada FizzBuzz que receba um número como parâmetro e retorne um array com os seguintes elementos:
+    
+    Se o número for múltiplo de 3: deve salvar "Fizz" no lugar do número
+    
+    Caso contrário, se o número for múltiplo de 5: deve salvar "Buzz" no lugar do número
+    
+    Caso contrário, se o número for múltiplo de 3 e 5: deve salvar "FizzBuzz" no lugar do número
+    
+    Caso contrário, salve somente o número
+*/
